3f7a964298c6ab54f3feeeefdb6d51553c660ff4
andyyou <andyyu0920@gmail.com>
2023-01-25 15:31:25 +0800

    feat: initialize with core function

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chrome Extension - GPT Markdown
+
+## Introduce
+
+> Warning: The version is still in experinment.
+
+GPT Markdown is a Chrome extension that support easy to download GPT conversation as markdown.
+
+## Development
+
+## Roadmap
+
+## Contribution
---
+++ content-script.js
@@ -0,0 +1,50 @@
+function main() {
+  if (!window.location.href.startsWith("https://chat.openai.com/chat")) {
+    return;
+  }
+
+  const mainElement = document.querySelector("main");
+
+  function handleClick(e) {
+    e.stopPropagation();
+
+    const turndownService = new TurndownService();
+    const html = mainElement.innerHTML;
+    const markdown = turndownService.turndown(html);
+
+    const blob = new Blob([markdown], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const el = document.createElement("a");
+    el.href = url;
+    const dt = new Date();
+    el.download = `GPT-${dt.getFullYear()}-${
+      dt.getMonth() + 1
+    }-${dt.getDate()}.md`;
+    el.click();
+    el.remove();
+  }
+
+  const prevElement = document.getElementById("gpt-markdown-export-button");
+  if (prevElement) {
+    prevElement.remove();
+  }
+
+  const btnElement = document.createElement("button");
+  btnElement.id = "gpt-markdown-export-button";
+  btnElement.textContent = "Export Markdown";
+  btnElement.addEventListener("click", handleClick);
+  mainElement.appendChild(btnElement);
+}
+
+function observe(cb) {
+  const observer = new MutationObserver(cb);
+  const root = document.querySelector("#__next");
+  observer.observe(root, {
+    attributes: false,
+    childList: true,
+    subtree: false,
+  });
+}
+
+main();
+observe(main);
